41f73cd07155f6fc950a721664abc3ef9fc0dc44
yukaueno <yuka_n@live.com>
2017-10-03 10:31:18 -0300

    Add buscacega.py

---
+++ buscacega.py
@@ -0,0 +1,40 @@
+# Busca Cega
+
+import sudoku_functions.py
+import numpy
+
+# board: sudoku a ser verificado
+# Verifica se satifaz as restricoes
+def sastify_restriction(board):
+	# Verifica as linhas
+		for i in range(0,9)
+			line =  board[i,:];
+			if  len(line) != len(set(line)):
+  			        return false
+ 
+	# Verifica as colunas
+		for i in range(0,9)
+                        column =  board[:,i];
+                        if  len(column) != len(set(column)):
+                                return false
+
+	# Verifica os bloquinhos 3x3
+
+
+# board: sudoku
+# Verifica se preencheu todos os dominios
+def solved(board)
+
+
+# board: sudoku a ser resolvido
+# Resolve o sudoku com busca cega
+def busca_cega(board):
+	 
+
+
+# Testa de 1 a 80
+for i in range(1,81)
+	board = numpy.array(random_puzzle(i))
+	 
+
+
